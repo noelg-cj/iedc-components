69254cf499468039a297a6e2d8995ad68686a313
Noel George <noelgeorge2004@gmail.com>
2023-06-29 13:40:52 +0530

    Made the contact info responsive

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import ContactInfo, {SocMedia} from './components/contact';
 function App() {
   return (
     <div className='mt-32'>
-      <div className="px-11">
-        <div className="bg-[#5658DD] px-8 py-16 flex justify-around max-w-2xl m-auto font-[Inter]">
+      <div className="px-6 py-8 flex justify-center">
+        <div className="bg-[#5658DD] px-4 py-12 flex flex-col gap-5 w-full justify-around font-[Inter] text-center min-[450px]:flex-row sm:text-left sm:px-12 sm:py-16">
           <ContactInfo name="Rizvan MS" desig="CEO" email="ceoiedc@cet.ac.in" />
           <ContactInfo name="Treasa Mariya" desig="PRO" email="proiedc@cet.ac.in" />
         </div>
--- src/components/contact.js
+++ src/components/contact.js
@@ -3,9 +3,9 @@ import {FaFacebookF, FaLinkedinIn, FaTwitter, FaInstagram} from 'react-icons/fa'
 
 const ContactInfo = (props) => {
     return (
-        <div>
-            <p className='text-[#DBEAFE] font-bold text-xl'>Contact: {props.name} ({props.desig})</p>
-            <p className='text-[#DBEAFE] text-xl'>Email: {props.email}</p>
+        <div className='flex flex-col'>
+            <p className='text-blue-100 font-semibold text-xl mb-1'>Contact: {props.name} ({props.desig})</p>
+            <p className='text-blue-100 text-xl mb-1'>Email: {props.email}</p>
         </div>
     )
 }
@@ -14,10 +14,10 @@ export const SocMedia = () => {
     return (
         <div>
             <div className='flex justify-between my-16 text-[#5658DD]'>
-                <FaFacebookF size={20} className='hover:text-[#DBEAFE] cursor-pointer' />
-                <FaLinkedinIn size={20} className='hover:text-[#DBEAFE] cursor-pointer' />
-                <FaTwitter size={20} className='hover:text-[#DBEAFE] cursor-pointer' />
-                <FaInstagram size={20} className='hover:text-[#DBEAFE] cursor-pointer' />
+                <FaFacebookF size={20} className='hover:text-[#ACADFF] cursor-pointer' />
+                <FaLinkedinIn size={20} className='hover:text-[#ACADFF] cursor-pointer' />
+                <FaTwitter size={20} className='hover:text-[#ACADFF] cursor-pointer' />
+                <FaInstagram size={20} className='hover:text-[#ACADFF] cursor-pointer' />
             </div>
         </div>
     )
