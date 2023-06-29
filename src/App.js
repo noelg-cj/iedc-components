3944cddd5a218274821492dff9221eecd125a95a
aharonmathews <aharonmathews04@gmail.com>
2023-06-29 18:49:04 +0530

    added ino

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@
 import React from 'react'
 /* import Greet from './components/Greet' */
 import ContactPanel, {Footer} from './components/contact';
+import Ino from './components/ino.js'
 
 function App() {
   return (
+    
+    
     <div className='mt-32'>
+      <div>
+      <Ino />
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+
       <div className="px-6 py-8 flex justify-center">
         <ContactPanel />
       </div>
